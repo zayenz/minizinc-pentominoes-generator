000e747c8ccbc7759430e12ed2f097eedb447af5
Mikael Zayenz Lagerkvist <research@zayenz.se>
2020-08-21 19:00:00 +0200

    Generate new instances with all four strategies

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-pwd
-
-for seed in 17 42 4711
+for strategy in source target close far
 do
-    for size in 5 10 15 20
-    do
-        for tiles in 5 10 15 20
-        do
-            ./target/release/minizinc-pentominoes-generator --seed $seed --size $size --tiles $tiles > ./data/size_${size}_tiles_${tiles}_seed_$seed.dzn
-        done
-    done
+  for seed in 17 42 4711
+  do
+      for size in 5 10 15 20
+      do
+          for tiles in 5 10 15 20
+          do
+              ./target/release/minizinc-pentominoes-generator --seed $seed --size $size --tiles $tiles > ./data/size_${size}_tiles_${tiles}_seed_${seed}_strategy_${strategy}.dzn
+          done
+      done
+  done
 done
